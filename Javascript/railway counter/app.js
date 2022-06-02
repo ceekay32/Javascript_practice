940d74bd72c5959b66e82b23fd47ac5b767fa25a
Chirag Khatri <chiragkhatri2001@gmail.com>
2022-06-02 22:44:04 +0530

    railway counter complete

--- Javascript/railway counter/app.js
+++ Javascript/railway counter/app.js
@@ -1,6 +1,7 @@
 let count = 0;
 console.log(count);
-
+let inc = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 // let myAge = 20;
 // console.log(myAge);
 
@@ -10,6 +11,14 @@ console.log(count);
 // console.log(myDogAge);
 function increment() {
   count = count + 1;
-  let inc = document.getElementById("count-el");
+
   inc.innerHTML = count;
+}
+
+function save() {
+  console.log(count);
+  let countStr = count + "-";
+  saveEl.textContent += countStr;
+  inc.innerHTML = 0;
+  count = 0;
 }
