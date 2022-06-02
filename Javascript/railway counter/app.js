cad4c448232c595e00e58ec219e7f72f807aff28
Chirag Khatri <chiragkhatri2001@gmail.com>
2022-06-02 12:26:47 +0530

    added increment button

--- Javascript/railway counter/app.js
+++ Javascript/railway counter/app.js
@@ -1,5 +1,15 @@
 let count = 0;
 console.log(count);
 
-let myAge = 20;
-console.log(myAge);
+// let myAge = 20;
+// console.log(myAge);
+
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+function increment() {
+  count = count + 1;
+  let inc = document.getElementById("count-el");
+  inc.innerHTML = count;
+}
